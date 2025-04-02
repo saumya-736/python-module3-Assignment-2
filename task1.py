16787fbb802fd48c46d54b125c79f922403383b0
saumya-736 <144018393+saumya-736@users.noreply.github.com>
2025-04-02 22:36:11 +0530

    Add files via upload
    task 1 and task 2 of module-3 assignment

---
+++ task1.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number :"))
+
+# Check if the number is even or odd
+if n%2 == 0:
+    print(n, "is an even number.")
+else:
+    print(n, "is an odd number.")
---
+++ task2.py
@@ -0,0 +1,7 @@
+total=0
+# Iterate over each element
+for i in range(1,51):
+    total +=  i
+
+# Print the sum
+print( " The sum of the  numbers from 1 to 50 is : " , total)
